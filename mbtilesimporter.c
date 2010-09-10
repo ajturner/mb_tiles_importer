b6bfa6b7b9a5712896ac4047b0a837ba85d5497f
Tom MacWright <tom@ubuntularge.(none)>
2010-09-10 17:25:21 -0400

    Fixing segfault

--- mbtilesimporter.c
+++ mbtilesimporter.c
@@ -31,23 +31,6 @@ void fail_with_help(char **argv) {
 ** "len" is the size of the data in the in[] buffer to encode.
 ** Return the number of bytes encoded, or -1 on error.
 */
-int bin2hex(char *out, const char *in, int len)
-{
-  int ct = len;
-  if (!in || !out || len < 0) return -1;
-
-  /* hexadecimal lookup table */
-  static char hex[] = "0123456789ABCDEF";
-
-  while (ct-- > 0)
-  {
-    *out   = hex[*in >> 4];
-    *out++ = hex[*in++ & 0x0F];
-  }
-
-  return len;
-}
-
 
 int main(int argc, char **argv){
   sqlite3 *db;
@@ -73,12 +56,13 @@ int main(int argc, char **argv){
   char* metafile;
   char* tile_source;
   char * formatpt;
+  char *mquery = "insert into tiles (zoom_level, tile_column, tile_row, tile_data) values (?1, ?2, ?3, ?4);";
 
   char query[8000];
-  char dirname[400];
-  char fname[400];
-  unsigned char buffer[10000];
-  char entyname[400];
+  char dirname[4000];
+  char fname[4000];
+  unsigned char buffer[100000];
+  char entyname[4000];
 
   while(1) {
     c = getopt (argc, argv, "f:m:M:s:d:");
@@ -137,21 +121,21 @@ int main(int argc, char **argv){
             while ((enty = readdir(diry)) != NULL) {
               if (strcmp(enty->d_name, ".") && strcmp(enty->d_name, "..")) { 
                 if (strstr(enty->d_name, format) != NULL) {
-                  printf(".");
                   strcpy(entyname, enty->d_name);
                   yvalue = strcspn(entyname, ".");
                   entyname[yvalue] = '\0';
                   sprintf(fname, "%s/%s/%s/%s", tile_source, entz->d_name, entx->d_name, enty->d_name);
+                  printf("(%s).\n", fname);
 
                   FILE * fp;
                   int i = 0;
                   long int filesize = 0;
                   static sqlite3_stmt *pStmtInsertBlob = NULL;
+                  buffer[0] = 0;
                   fp = fopen(fname, "r");
-                  for (i = 0; (rc = getc(fp)) != EOF && i < 10000; buffer[i++] = rc);
+                  for (i = 0; (rc = getc(fp)) != EOF && i < 1000000; buffer[i++] = rc);
 
-                  char *mquery = "insert into tiles (zoom_level, tile_column, tile_row, tile_data) values (?1, ?2, ?3, ?4);";
-
+                  printf("(%s) read.\n", fname);
                   if(sqlite3_prepare_v2(db, mquery, -1, &pStmtInsertBlob, 0) != SQLITE_OK)
 	                {
 	                        printf("Could not prepare INSERT blob statement.\n");
@@ -161,6 +145,7 @@ int main(int argc, char **argv){
                   z = atoi(entz->d_name);
                   x = atoi(entx->d_name);
                   y = atoi(entyname);
+                  printf("(%s) exec.\n", fname);
 
                   rc = sqlite3_exec(db, mquery, NULL, 0, &zErrMsg);
 
@@ -178,9 +163,10 @@ int main(int argc, char **argv){
                   }
 
                   if((rc = sqlite3_step(pStmtInsertBlob)) != SQLITE_DONE)
-                    printf("error\n");
+                    printf("blob prob error\n");
                   else
                     sqlite3_reset(pStmtInsertBlob);
+                  fclose(fp);
                 }
               }
             }
